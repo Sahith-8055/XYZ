70c5d42de6fd2e091954d582d792fdb62d9bab3b
Sahith-8055 <sahithyerram007@msitprogram.net>
2018-08-03 19:34:43 +0530

    " -> 0 of 3 passed. pylint: 10.00/10 "

---
+++ CSPP1-ASSIGNMENTS/M5/p4/square_root_newtonrapson.py
@@ -0,0 +1,13 @@
+"""Write a python program to find the square root of the given number using newton raphson method"""
+def main():
+    """Writing inside the function"""
+    squar_root = int(input())
+    epsilon = 0.01
+    guess = squar_root/2.0
+    num_guesses = 0
+    while abs(guess**2 - squar_root) >= epsilon:
+        num_guesses += 1
+        guess = guess - (((guess**2) - squar_root)/(2*guess))
+    print('Square root of ' + str(squar_root) + ' is about ' + str(guess))
+if __name__ == "__main__":
+    main()
