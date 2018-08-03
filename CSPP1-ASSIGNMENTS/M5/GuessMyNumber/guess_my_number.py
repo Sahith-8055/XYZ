20ab10b1243a0459861744b83a2370d1a28bb5f2
Sahith-8055 <sahithyerram007@msitprogram.net>
2018-08-03 17:34:44 +0530

    guess_my_number.py

--- CSPP1-ASSIGNMENTS/M5/GuessMyNumber/guess_my_number.py
+++ CSPP1-ASSIGNMENTS/M5/GuessMyNumber/guess_my_number.py
@@ -1,21 +1,25 @@
-"""Guess a number"""
-LOW_INP = 0
-HIGH_INP = 100
-MIDDLE_INP = (LOW_INP + HIGH_INP)/2
-GUESS_I = 'h'
-while GUESS_I != 'c':
-    print("Is your secret number", MIDDLE_INP, "?")
-    print("Enter 'h' to indicate guess is too high.")
-    print("Enter 'l' to indicate the guess is too low.")
-    print("Enter 'c' to indicate I guessed correctly.")
-    GUESS_I = input()
-    if GUESS_I == 'h':
-        HIGH_INP = MIDDLE_INP
-        MIDDLE_INP = int((LOW_INP + HIGH_INP)/2)
-    if GUESS_I == 'l':
-        LOW_INP = MIDDLE_INP
-        MIDDLE_INP = int((LOW_INP + HIGH_INP)/2)
-    if GUESS_I != 'c':
-        print("Sorry, I didn't understand your input")
-print("Game Over")
-print("Your secret number is ", MIDDLE_INP)
+"""Guess My Number Exercise"""
+def main():
+    """Writing the function"""
+    low_inp = 0
+    high_inp = 100
+    middle_inp = (low_inp + high_inp)/2
+    guess_i = 'h'
+    while guess_i != 'c':
+        print("Is your secret number", middle_inp, "?")
+        print("Enter 'h' to indicate guess is too high.")
+        print("Enter 'l' to indicate the guess is too low.")
+        print("Enter 'c' to indicate I guessed correctly.")
+        guess_i = input()
+        if guess_i == 'h':
+            high_inp = middle_inp
+            middle_inp = int((low_inp + high_inp)/2)
+        if guess_i == 'l':
+            low_inp = middle_inp
+            middle_inp = int((low_inp + high_inp)/2)
+        if guess_i != 'c':
+            print("Sorry, I didn't understand your input")
+    print("Game Over")
+    print("Your secret number is ", middle_inp)
+if __name__ == "__main__":
+    main()
