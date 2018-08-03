bd409ee7c2772ab92747d3d72699298f94259d2f
Sahith-8055 <sahithyerram007@msitprogram.net>
2018-08-03 18:29:18 +0530

    " -> 0 of 3 passed. pylint: 10.00/10 "

---
+++ CSPP1-ASSIGNMENTS/M5/p2/square_root.py
@@ -0,0 +1,18 @@
+"""Write a python program to find the square root of the given number"""
+def main():
+    """Writing inside this function"""
+    squ_apprx = int(input())
+    epsilon_1 = 0.01
+    guess_1 = 0.0
+    step = 0.1
+    num_guesses = 0
+    while abs(guess_1**2 - squ_apprx) >= epsilon_1 and guess_1 <= squ_apprx:
+        guess_1 += step
+        num_guesses += 1
+    print('num_guesses =', num_guesses)
+    if abs(guess_1**2 - squ_apprx) >= epsilon_1:
+        print('Failed on square root of', squ_apprx)
+    else:
+        print(guess_1, 'is close to the cube root of', squ_apprx)
+if __name__ == "__main__":
+    main()
