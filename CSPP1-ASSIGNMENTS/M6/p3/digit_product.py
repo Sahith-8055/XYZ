e43a2615adab6895c6b9b720ff6f0fe74c2d43c1
Sahith-8055 <sahithyerram007@msitprogram.net>
2018-08-04 11:13:37 +0530

    " -> 5 of 7 passed. pylint: 9.29/10 "

--- CSPP1-ASSIGNMENTS/M6/p3/digit_product.py
+++ CSPP1-ASSIGNMENTS/M6/p3/digit_product.py
@@ -9,13 +9,16 @@ def main():
     Read any number from the input, store it in variable int_input.
     '''
     int_input = int(input())
-    add = 1
-    i = 1
-    while i <= abs(int_input):
-        rem = (abs(int_input))%10
-        add = add*rem
-        int_input = abs(int_input)//10
-        i = i + 1
-    print(add)
+    if int_input == 0:
+        print(0)
+    else:    
+        add = 1
+        i = 1
+        while i <= abs(int_input):
+            rem = (abs(int_input))%10
+            add = add*rem
+            int_input = abs(int_input)//10
+            i = i + 1
+        print(add)
 if __name__ == "__main__":
     main()
