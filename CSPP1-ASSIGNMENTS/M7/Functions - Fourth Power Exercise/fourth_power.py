40d8c99485bf108abcdf9b4389abac6deb4d543c
Sahith-8055 <sahithyerram007@msitprogram.net>
2018-08-06 19:27:03 +0530

    " -> 0 of 10 passed. pylint: 10.00/10 "

--- CSPP1-ASSIGNMENTS/M7/Functions - Fourth Power Exercise/fourth_power.py
+++ CSPP1-ASSIGNMENTS/M7/Functions - Fourth Power Exercise/fourth_power.py
@@ -1,11 +1,7 @@
 # Exercise: fourth power
-
 # Write a Python function, fourthPower, that takes in one number and returns that value raised to the fourth power.
-
 # You should use the square procedure that you defined in an earlier exercise exercise (you don't need to redefine square in this box;
-
 # This function takes in one number and returns one number.
-
 def square(x):
     '''
     x: int or float.
