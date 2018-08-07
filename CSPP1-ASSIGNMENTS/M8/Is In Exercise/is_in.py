396ec52d87883b02d01834570bd14b9b5d6d0f40
Sahith-8055 <sahithyerram007@msitprogram.net>
2018-08-07 18:29:36 +0530

    is_in.py

--- CSPP1-ASSIGNMENTS/M8/Is In Exercise/is_in.py
+++ CSPP1-ASSIGNMENTS/M8/Is In Exercise/is_in.py
@@ -5,8 +5,23 @@ def isIn(char, aStr):
     aStr: an alphabetized string
     returns: True if char is in aStr; False otherwise
     '''
-    
+    length_str = len(aStr)
+    middle_char = length_str // 2
+    if length_str == 0:
+        return False
+    if length_str == 1:
+        if char == aStr:
+            return True
+        else:
+            return False
+    if aStr[middle_char] == char:
+        return True
+    elif aStr[middle_char] > char:
+        return isIn(char,aStr[0:middle_char])
+    else:
+        return isIn(char,aStr[middle_char:length_str+1])
 def main():
+    ''' Writing inside this function'''
     data = input()
     data = data.split()
     print(isIn((data[0][0]), data[1]))
