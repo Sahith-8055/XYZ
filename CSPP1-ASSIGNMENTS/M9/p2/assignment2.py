976768af1f92b5321472a705a1afca9f05a94b9c
Sahith-8055 <sahithyerram007@msitprogram.net>
2018-08-08 17:40:17 +0530

    " -> 9 of 9 passed. pylint: 10.00/10 "

---
+++ CSPP1-ASSIGNMENTS/M9/p2/assignment2.py
@@ -0,0 +1,33 @@
+"""Exercise: Assignment-2"""
+def get_guessed_word(secret_word, letters_guessed):
+    '''
+    secret_word: string, the word the user is guessing
+    letters_guessed: list, what letters have been guessed so far
+    returns: string, comprised of letters and underscores that represents
+      what letters in secret_word have been guessed so far.
+    '''
+    k_inp = ''
+    for char in secret_word:
+        if char in letters_guessed:
+            k_inp = k_inp + char
+        else:
+            k_inp = k_inp + '_'
+    return k_inp
+
+def main():
+    '''
+    Main function for current assignment
+    '''
+    user_input = input()
+    if user_input:
+        data = user_input.split()
+        secret_word = data[0]
+    else:
+        data = []
+        secret_word = ""
+    list1 = []
+    for j in range(1, len(data)):
+        list1.append(data[j][0])
+    print(get_guessed_word(secret_word, list1))
+if __name__ == "__main__":
+    main()
