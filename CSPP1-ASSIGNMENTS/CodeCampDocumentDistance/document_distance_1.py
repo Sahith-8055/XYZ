5a6e69a2174b187a9dfa41e4bc968cba2a4e9d90
Sahith-8055 <sahithyerram007@msitprogram.net>
2018-08-17 18:25:29 +0530

    " -> 3 of 3 passed. pylint: 9.51/10 "

--- CSPP1-ASSIGNMENTS/CodeCampDocumentDistance/document_distance_1.py
+++ CSPP1-ASSIGNMENTS/CodeCampDocumentDistance/document_distance_1.py
@@ -3,36 +3,38 @@
 '''
 import math
 import re
-stopwords = "stopwords.txt"
+STOPWORDS = "stopwords.txt"
 def cleanup_words(input1):
+    '''Cleaning up the words'''
     reg = re.compile('[^a-z]')
     input1 = input1.lower()
-    input1 = [reg.sub('',w.strip()) for w in input1.split(' ')]
+    input1 = [reg.sub('', w.strip()) for w in input1.split(' ')]
     return input1
-def remove_stopwords(input1,input2):
-    d={}
-    d=load_stopwords(stopwords)
+def remove_stopwords(input1, input2):
+    '''Remove the unwanted words'''
+    d_input = {}
+    d_input = load_stopwords(STOPWORDS)
     word_list1 = cleanup_words(input1)
     word_list2 = cleanup_words(input2)
-    word_list  = word_list1 + word_list2
+    word_list = word_list1 + word_list2
     dictionary = {}
     for  word in word_list:
-        if word not in d and len(word) > 0:
-            dictionary[word] = (word_list1.count(word),word_list2.count(word))
-    return dictionary        
+        if word not in d_input and len(word) > 0:
+            dictionary[word] = (word_list1.count(word), word_list2.count(word))
+    return dictionary
 def similarity(dict1, dict2):
     '''
         Compute the document distance as given in the PDF
     '''
     dictionary = {}
-    dictionary = remove_stopwords(dict1,dict2)
+    dictionary = remove_stopwords(dict1, dict2)
     numerator, denominator1, denominator2 = 0, 0, 0
-    for d in dictionary.values():    
-        numerator += d[0] * d[1]
-        denominator1 += (d[0]**2)
-        denominator2 += (d[1]**2)
+    for k in dictionary.values():
+        numerator += k[0] * k[1]
+        denominator1 += (k[0]**2)
+        denominator2 += (k[1]**2)
         denominator = math.sqrt(denominator1) * math.sqrt(denominator2)
-    return (numerator / denominator)
+    return numerator / denominator
 def load_stopwords(filename):
     '''
         loads stop words from a file and returns a dictionary
