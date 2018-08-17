74f1d97306573702cb8ec98c264bae7720c013bb
Sahith-8055 <sahithyerram007@msitprogram.net>
2018-08-17 18:32:25 +0530

    " -> 3 of 3 passed. pylint: 9.51/10 "

--- CSPP1-ASSIGNMENTS/CodeCampDocumentDistance/document_distance.py
+++ CSPP1-ASSIGNMENTS/CodeCampDocumentDistance/document_distance.py
@@ -1,53 +1,55 @@
-"""DOCUMENTDISTANCE"""
+'''
+    Document Distance - A detailed description is given in the PDF
+'''
 import math
-FILENAME = "stopwords.txt"
+import re
+STOPWORDS = "stopwords.txt"
+def cleanup_words(input1):
+    '''Cleaning up the words'''
+    reg = re.compile('[^a-z]')
+    input1 = input1.lower()
+    input1 = [reg.sub('', w.strip()) for w in input1.split(' ')]
+    return input1
+def remove_stopwords(input1, input2):
+    '''Remove the unwanted words'''
+    d_input = {}
+    d_input = load_stopwords(STOPWORDS)
+    word_list1 = cleanup_words(input1)
+    word_list2 = cleanup_words(input2)
+    word_list = word_list1 + word_list2
+    dictionary = {}
+    for  word in word_list:
+        if word not in d_input and len(word) > 0:
+            dictionary[word] = (word_list1.count(word), word_list2.count(word))
+    return dictionary
 def similarity(dict1, dict2):
-    """DISTANCE"""
-    list_1 = ''
-    for i in dict1:
-        for j in i:
-            if j not in '!@#$%^&*()_+-=,.?1234567890':
-                if j not in "'":
-                    list_1 = list_1 + j
-    list_2 = ''
-    for i in dict2:
-        for j in i:
-            if j not in '!@#$%^&*()_+-=",.?1234567890':
-                if j not in "'":
-                    list_2 = list_2 + j
-
-    list_1 = list_1.split()
-    list_2 = list_2.split()
-    lis3 = lis_1 + lis_2
-    dict3 = {}
-    for word in lis3:
-        if word not in load_stopwords(FILENAME).keys():
-            dict3[word] = (lis1.count(word), lis2.count(word))
-    numerator = 0
-    denominator = 0
-    sum1 = 0
-    sum2 = 0
-    for i in dict3:
-        numerator = numerator + (dict3[i][0] * dict3[i][1])
-        sum1 = sum1 + dict3[i][0] ** 2
-        sum2 = sum2 + dict3[i][1] ** 2
-    denominator = math.sqrt(sum1) * math.sqrt(sum2)
-    similarity1 = (numerator / denominator)
-    return similarity1
+    '''
+        Compute the document distance as given in the PDF
+    '''
+    dictionary = {}
+    dictionary = remove_stopwords(dict1, dict2)
+    numerator, denominator1, denominator2 = 0, 0, 0
+    for k in dictionary.values():
+        numerator += k[0] * k[1]
+        denominator1 += (k[0]**2)
+        denominator2 += (k[1]**2)
+        denominator = math.sqrt(denominator1) * math.sqrt(denominator2)
+    return numerator / denominator
 def load_stopwords(filename):
-    """DISTABCE"""
+    '''
+        loads stop words from a file and returns a dictionary
+    '''
     stopwords = {}
     with open(filename, 'r') as filename:
         for line in filename:
             stopwords[line.strip()] = 0
     return stopwords
-
 def main():
-    """DISTANCE"""
+    '''
+        take two inputs and call the similarity function
+    '''
     input1 = input().lower()
     input2 = input().lower()
-
     print(similarity(input1, input2))
-
 if __name__ == '__main__':
     main()
