98eb95778b2382e90316518288a8a596a041bb17
Sahith-8055 <sahithyerram007@msitprogram.net>
2018-08-25 13:24:58 +0530

    " -> 0 of 26 passed. pylint: 9.05/10 "

--- CSPP1-ASSIGNMENTS/M22/Check Sudoku/check_sudoku.py
+++ CSPP1-ASSIGNMENTS/M22/Check Sudoku/check_sudoku.py
@@ -3,27 +3,32 @@
     The objective is to fill a 9×9 grid with digits so that
     each column, each row, and each of the nine 3×3 subgrids that compose the grid
     contains all of the digits from 1 to 9.
-
     Complete the check_sudoku function to check if the given grid
     satisfies all the sudoku rules given in the statement above.
 '''
-
 def check_sudoku(sudoku):
     '''
         Your solution goes here. You may add other helper functions as needed.
         The function has to return True for a valid sudoku grid and false otherwise
     '''
-    pass
-
+    for i in sudoku:
+        for j in i:
+            if j not in '1234567890':
+                return False
+    return True
+def count(sudoku):
+    for i in sudoku:
+        for j in i:
+            if i.count(j) != 1 and i.count(j) >= 1:
+                return False
+    return True
 def main():
     '''
         main function to read input sudoku from console
         call check_sudoku function and print the result to console
     '''
-    
     # initialize empty list
     sudoku = []
-
     # loop to read 9 lines of input from console
     for i in range(9):
         # read a line, split it on SPACE and append row to list
@@ -31,6 +36,6 @@ def main():
         sudoku.append(row)
     # call solution function and print result to console
     print(check_sudoku(sudoku))
-
+    print(count(sudoku))
 if __name__ == '__main__':
     main()
