b22ba0ecf17877aa33e426d4d3e808fc4686ee9b
Sahith-8055 <sahithyerram007@msitprogram.net>
2018-08-25 11:29:16 +0530

    " -> 2 of 3 passed. pylint: 10.00/10 "

--- CSPP1-ASSIGNMENTS/M22/assignment2/clean_input.py
+++ CSPP1-ASSIGNMENTS/M22/assignment2/clean_input.py
@@ -2,10 +2,12 @@
 Write a function to clean up a given string by removing the special characters and retain
 alphabets in both upper and lower case and numbers.
 '''
+import re
 def clean_string(string):
     '''Clean string function'''
-    string = ''.join([i for i in string if i.isalpha()])
-    return string
+    regex = re.compile('[^a-zA-z0-9]')
+    cleared_string = regex.sub('', string)
+    return cleared_string
 def main():
     '''Main function'''
     string = input()
---
+++ CSPP1-ASSIGNMENTS/M22/assignment3/tokenize.py
@@ -0,0 +1,13 @@
+'''
+Write a function to tokenize a given string and return a dictionary with the frequency of
+each word
+'''
+def tokenize(string):
+    dictionary1 = {}
+    for keys, values in enumerate(dictionary1):
+    	    
+def main():
+    dictionary = {}
+
+if __name__ == '__main__':
+    main()
