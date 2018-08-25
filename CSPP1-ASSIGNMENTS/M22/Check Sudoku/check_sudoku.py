a84b7a8a407908f618fdc7c53cfc0bec5dc931af
Sahith-8055 <sahithyerram007@msitprogram.net>
2018-08-25 13:25:47 +0530

    " -> 5 of 26 passed. pylint: 9.50/10 "

--- CSPP1-ASSIGNMENTS/M22/Check Sudoku/check_sudoku.py
+++ CSPP1-ASSIGNMENTS/M22/Check Sudoku/check_sudoku.py
@@ -30,7 +30,7 @@ def main():
     # initialize empty list
     sudoku = []
     # loop to read 9 lines of input from console
-    for i in range(9):
+    for _ in range(9):
         # read a line, split it on SPACE and append row to list
         row = input().split(' ')
         sudoku.append(row)
